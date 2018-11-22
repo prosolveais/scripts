d7b20685f8a847f906d7ed474d1bf0577a0cf23c
Syaafiq Asyraf A. S <33458564+syaraf-az@users.noreply.github.com>
2018-11-22 18:35:10 +0800

    update firewall script to resolve domain instead of using hardcoded ip

--- script.sh
+++ script.sh
@@ -1,33 +1,35 @@
-#!/usr/bin/env bash
+#######################################
+### PROSOLVE iptables configuration ###
+#######################################
 
-if (( $EUID != 0 )); then
-    echo "Please run as root"
-    exit
-fi
-
-# clear existing iptables
-iptables -X
+# 0: drop all policy
 iptables -F
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
+ip6tables -F
 
-# for loopback device
+# 1: allow loopback connection
 iptables -I INPUT 1 -i lo -j ACCEPT
+ip6tables -I INPUT 1 -i lo -j ACCEPT
+iptables -I OUTPUT 1 -o lo -j ACCEPT
+ip6tables -I OUTPUT 1 -o lo -j ACCEPT
 
-# prosolve server
-printf "147.75.94.253\tprosolve.ml\n" >> /etc/hosts
-iptables -A INPUT -s 147.75.94.253 -j ACCEPT
-iptables -A OUTPUT -d 147.75.94.253 -j ACCEPT
+# 3: allow dns connection
+iptables -A OUTPUT -p UDP --dport 53 -j ACCEPT
+ip6tables -A OUTPUT -p UDP --dport 53 -j ACCEPT
 
-# for systemd-resolved's stub resolver
-iptables -A INPUT -s 127.0.0.53 -j ACCEPT
-iptables -A OUTPUT -d 127.0.0.53 -j ACCEPT
-
-# drop all other connections
+# 2: allow connection the have been made by this pc
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
+ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+ip6tables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# final step
-systemctl restart systemd-resolved
+# 4: allow connection to prosolve.ml
+iptables -A OUTPUT -p tcp -d prosolve.ml -m multiport --dport 80,443 -j ACCEPT
+ip6tables -A OUTPUT -p tcp -d prosolve.ml -m multiport --dport 80,443 -j ACCEPT
+
+# 5: drop everything
+iptables -P INPUT DROP
+ip6tables -P INPUT DROP
+iptables -P FORWARD DROP
+ip6tables -P FORWARD DROP
+iptables -P OUTPUT DROP
+ip6tables -P OUTPUT DROP
