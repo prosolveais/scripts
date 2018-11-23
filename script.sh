13cc2ea640c9479c13a24210f32271194472b27d
Syaafiq Asyraf A. S <33458564+syaraf-az@users.noreply.github.com>
2018-11-23 16:52:55 +0800

    allow ntp connection

--- script.sh
+++ script.sh
@@ -12,21 +12,25 @@ ip6tables -I INPUT 1 -i lo -j ACCEPT
 iptables -I OUTPUT 1 -o lo -j ACCEPT
 ip6tables -I OUTPUT 1 -o lo -j ACCEPT
 
-# 3: allow dns connection
+# 2: allow dns connection
 iptables -A OUTPUT -p UDP --dport 53 -j ACCEPT
 ip6tables -A OUTPUT -p UDP --dport 53 -j ACCEPT
 
-# 2: allow connection the have been made by this pc
+# 3: allow ntp connection
+iptables -A OUTPUT -p UDP --dport 123 -j ACCEPT
+ip6tables -A OUTPUT -p UDP --dport 123 -j ACCEPT
+
+# 4: allow connection the have been made by this pc
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 ip6tables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# 4: allow connection to prosolve.ml
+# 5: allow connection to prosolve.ml
 iptables -A OUTPUT -p tcp -d prosolve.ml -m multiport --dport 80,443 -j ACCEPT
 ip6tables -A OUTPUT -p tcp -d prosolve.ml -m multiport --dport 80,443 -j ACCEPT
 
-# 5: reject everything
+# 6: reject everything
 iptables -A INPUT -j REJECT
 ip6tables -A INPUT -j REJECT
 iptables -A OUTPUT -j REJECT
